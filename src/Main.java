88292a70186ea7afa9400d8640e92a9a3a394556
hugo <hugo@MacBook-Pro-de-hugo.local>
2017-05-05 15:21:56 -0400

    init commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Port number: ");
+        int port = reader.nextInt();
+    }
+}
